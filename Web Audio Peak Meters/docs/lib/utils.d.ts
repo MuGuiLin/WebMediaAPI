0b9fe613c764387f22f3b261952b446f31dc9ec7
muguilin <muguilin@foxmail.com>
2023-12-04 12:29:59 +0800

    update

--- Web Audio Peak Meters/docs/lib/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function getBaseLog(x: number, y: number): number;
-export declare function dbFromFloat(floatVal: number): number;
-export declare function findAudioProcBufferSize(numSamplesIn: number): number;
-export declare function dbTicks(min: number, max: number, tickSize: number): number[];
-export declare function testSignalGenerator(hz: number, rotation?: number, sampleRate?: number): number[];
-export declare function offsetSincGenerator(): number[];
