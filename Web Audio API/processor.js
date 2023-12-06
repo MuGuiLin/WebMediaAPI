f59f330bfd69887b98593db6b861be09a7bf43bd
muguilin <muguilin@foxmail.com>
2023-12-06 17:52:13 +0800

    update

--- Web Audio API/processor.js
+++ Web Audio API/processor.js
@@ -1,5 +1,4 @@
-// processor.js
-function peakValues(input) {
+function countMaxValue(input) {
     return input.map(channel => {
         let max = 0;
         for (let s = 0; s < channel.length; s++) {
@@ -12,9 +11,13 @@ function peakValues(input) {
 
 class RandomNoiseProcessor extends AudioWorkletProcessor {
     process(inputs, outputs, parameters) {
-        this.port.postMessage({ type: 'peaks', input: peakValues(inputs[0]) });
+        this.port.postMessage(countMaxValue(inputs[0]));
         return true;
     }
 };
 
-registerProcessor("mu-processor", RandomNoiseProcessor);
+try {
+    registerProcessor("mu-processor", RandomNoiseProcessor);
+} catch (error) {
+    console.log('无法注册峰值样本处理器。这可能意味着它已经注册了。', error);
+}
